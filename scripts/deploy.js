60c13dc17853585bcb4402e76c980ee54f1f5a38
0xFatih <98255385+FatihX1X@users.noreply.github.com>
2026-06-24 16:30:34 +0300

    Require treasury when deploying messenger

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,20 @@
 const hre = require('hardhat');
 
 async function main() {
+  const treasury = process.env.TREASURY_ADDRESS;
+
+  if (!treasury) {
+    throw new Error('TREASURY_ADDRESS is required');
+  }
+
   const Messenger = await hre.ethers.getContractFactory('ArcanumMessenger');
-  const messenger = await Messenger.deploy();
+  const messenger = await Messenger.deploy(treasury);
 
   await messenger.waitForDeployment();
 
   const address = await messenger.getAddress();
   console.log(`ArcanumMessenger deployed to ${address}`);
+  console.log(`Treasury address: ${treasury}`);
   console.log(`Set NEXT_PUBLIC_CONTRACT_ADDRESS=${address}`);
 }
 
